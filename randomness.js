e77932e420fb287b803354590e4fd6ea4400c69e
mahdiASC <mahdi@allstarcode.org>
2018-06-05 11:58:19 -0400

    final

--- randomness.js
+++ randomness.js
@@ -1,1 +1,51 @@
+//////////////////////
+// GLOBAL VARIABLES //
+//////////////////////
+const alphabet = "abcdefghijklmnopqrstuvwxyz";
+const foods = [
+    "pizza",
+    "burgers",
+    "rice and beans",
+    "lasagna",
+    "macaroni and cheese",
+    "baked potatoes"
+];
+const students = [
+    "Bob",
+    "Martin",
+    "Danny",
+    "Mahdi",
+    "Harry",
+    "Sean",
+    "Robert"
+];
+/////////////////////////////
+// END OF GLOBAL VARIABLES //
+/////////////////////////////
+
+
+// should console.log a random number between 0 and 21 (exclusively)
+function randomNumber(){
+
+}
+
+// should console.log a random letter from the alphabet
+function randomLetter(){
+
+}
+
+// should console.log a random food from the "foods" array
+function randomFood(){
+
+}
+
+// should console.log a random student from the "students" array
+function randomStudent(){
+
+}
+
+// should console.log three random students from the "students" array
+function randomGroup(){
+
+}
 
